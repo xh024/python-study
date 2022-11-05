a0b3b909aac3a1417b39b65b36791465d50cd263
jeongho <jeongho4781@gmail.com>
2022-11-06 02:11:55 +0900

    파일 추가

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Youtube 테크보이 워니 강좌 내용 정리
 
+
 ### 유튜브 채널
 - https://www.youtube.com/c/TeccboiWonie
+
 
 ### 강좌명 - 초단기 속성코딩(파이썬)
 - introduce : https://youtu.be/roItkTQ7t3M
@@ -12,5 +14,15 @@
 - 4강 : https://youtu.be/xdVt6qbghzI
 - 5강 : https://youtu.be/SeZr0VYozCM
 
+
+
 ### 그 외
 #부터는 강의 외 연습 파일
+
+
+
+### 초보자를 위한 파이썬 300제
+
+![링크](https://wikidocs.net/images//book/python300_zoR1Cof.PNG)
+- 도서 : https://wikidocs.net/book/922
+- 강의 : https://www.youtube.com/c/pystock
---
+++ 초보자를 위한 파이썬 300제/#001 ~ 010.py
@@ -0,0 +1,43 @@
+# 001
+print("Hello World")
+
+# 002
+print("Mary's cosmetics")
+
+# 003
+print('신씨가 소리질렀다. "도둑이야".')
+
+# 004
+print('"C:\Windows"')
+
+# 005
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+# \n == 줄 바꿈, \t == 탭
+
+# 006
+print("오늘은", "일요일")
+# 오늘은 일요일
+
+
+"""# 007"""
+print("naver", "kakao", "sk", "samsung", sep = ";")
+# sep인자를 사용하면 출력되는 값들 사이에 공백대신 세미콜론이 들어가게 된다
+
+
+# 008
+print("naver", "kakao", "sk", "samsung", sep = "/")
+
+
+"""# 009"""
+print("first");print("second")
+# 한 줄에 두개의 명령어를 적고 싶으면 print() 사이에 ;(세미콜론)을 사용한다
+print("first", end="");print("second")
+
+# 한 줄이 아닌 여러줄로 나눠 쓸 수 있다
+print("first", end = "")
+print("second", end="")
+print("third")
+
+
+# 010
+print(5/3)
